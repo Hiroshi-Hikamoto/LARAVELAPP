424da98865e61c29b664a242efeb2927361cf237
Hiroshi <perezcastro.1228@gmail.com>
2023-10-11 17:56:37 -0500

    Se coloco nuevo logo y se empezo a personalizar la apariencia

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset('images/arroyave/AVAPP2-02.ico') }}">
+    <link rel="icon" href="{{ asset('images/icons/LOGOICOAIE.ico') }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
@@ -47,34 +47,36 @@
   
     {{-- <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.1/js/jquery.dataTables.js"></script> --}}
 
+    <style>
+        .navbarverde {
+            background: #B3E7BF;
+            }
+    </style>
     
 </head>
 <body>
     <div id="app">
     @if (Auth::check())
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light navbarverde  shadow-sm">
         @else
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light navbarverde shadow-sm">
         @endif    
+        <img src="{{ asset('/images/icons/LOGOAIEICO.ico') }}" width="70" height="70" class="d-inline-block align-top" alt="">
             
         <a class="navbar-brand" href="{{ url('/home') }}">
-            {{-- <img src="{{ asset('/images/arroyave/logo.png') }}" class="logo"  style="height: 80px"/> --}}
-            {{ config('app.name', 'Laravel') }}
+            <!-- {{ config('app.name', 'Laravel') }} -->
         </a>
             <div class="container">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+            
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
 
                     @if (Auth::check())
-                    
 
                     <ul class="navbar-nav mr-auto">
-                        <a class="btn btn-primary btn-sm" href="{{ route('testigos.index') }}"> {{ __('Gestion de Testigos') }}</a>  
-
                         
                         @if (Auth::user()->perfil != 3)
                             {{-- <a class="nav-link" href="{{ route('afiliados.index') }}">{{ __('Listado') }}</a>  
@@ -127,6 +129,8 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                    <a class="btn btn-warning btn-rounded btn-lg float-right" href="{{ route('testigos.index') }}"> {{ __('Gestion de Testigos') }}</a>  
+
                         <!-- Authentication Links -->
                         @if (Auth::check())
                         <li class="nav-item">
